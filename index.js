3458a8ccff8c32e2d545b09a5dfc173f0691b7ef
Nageshwar <nageshpawarpatil@gmail.com>
2024-08-19 19:46:10 +0530

    changes made

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const CreatorModel = require("./models/creatorModel");
+const databaseConnection = require("./config/db.config");
 const PORT = process.env.PORT || 5000;
 
 

38be0ec408326075cab2610b577e7aedbf17395c
Nageshwar <nageshpawarpatil@gmail.com>
2024-08-19 16:42:05 +0530

    changes made

--- models/createTodo.model.js
+++ models/createTodo.model.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const todoSchema = require("../schemas/todo.schema");
 
-const TodoModel = mongoose.model("Todo", todoSchema);
+const TodoModel = mongoose.model("Creator", todoSchema);
 
 module.exports = TodoModel;

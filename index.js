cabbf58cedf3f36942467ad3f2c685143c47c247
Nageshwar <nageshpawarpatil@gmail.com>
2024-08-19 20:18:44 +0530

    asjkhjhkd

--- index.js
+++ index.js
@@ -201,56 +201,56 @@ app.get("/creator/:id", async (req, res) => {
   }
 });
 
-// app.get("/search", async (req, res) => {
-//   try {
-//     const { q, language, education, specialization } = req.query;
+app.get("/search", async (req, res) => {
+  try {
+    const { q, language, education, specialization } = req.query;
 
-//     const query = {};
+    const query = {};
 
-//     if (q) {
-//       const regex = new RegExp(q, "i");
-//       query.$or = [
-//         { name: regex },
-//         { email: regex },
-//         { specializations: regex },
-//       ];
-//     }
+    if (q) {
+      const regex = new RegExp(q, "i");
+      query.$or = [
+        { name: regex },
+        { email: regex },
+        { specializations: regex },
+      ];
+    }
 
-//     if (language) {
-//       // query.languages = { $in: [language] };
-//       query.languages = { $elemMatch: { $regex: new RegExp(language, "i") } };
-//     }
+    if (language) {
+      // query.languages = { $in: [language] };
+      query.languages = { $elemMatch: { $regex: new RegExp(language, "i") } };
+    }
 
-//     if (education) {
-//       // query.education = education;
-//       if (education) {
-//         query.education = { $regex: new RegExp(education, "i") };
-//       }
-//     }
+    if (education) {
+      // query.education = education;
+      if (education) {
+        query.education = { $regex: new RegExp(education, "i") };
+      }
+    }
 
-//     if (specialization) {
-//       // query.specializations = { $in: [specialization] };
-//       query.specializations = {
-//         $elemMatch: { $regex: new RegExp(specialization, "i") },
-//       };
-//     }
+    if (specialization) {
+      // query.specializations = { $in: [specialization] };
+      query.specializations = {
+        $elemMatch: { $regex: new RegExp(specialization, "i") },
+      };
+    }
 
-//     const creators = await CreatorModel.find(query);
+    const creators = await CreatorModel.find(query);
 
-//     res.json({
-//       message: "Search Creators List",
-//       success: true,
-//       error: false,
-//       creators,
-//     });
-//   } catch (error) {
-//     res.json({
-//       message: error.message || error,
-//       success: false,
-//       error: true,
-//     });
-//   }
-// });
+    res.json({
+      message: "Search Creators List",
+      success: true,
+      error: false,
+      creators,
+    });
+  } catch (error) {
+    res.json({
+      message: error.message || error,
+      success: false,
+      error: true,
+    });
+  }
+});
 
 // app.patch("/update_creator/:id", async (req, res) => {
 //   try {
